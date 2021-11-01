55aea1d57ba85dd84c116ebb608296d13781561f
Corentin TALOUR <corentin.talour@bts-malraux.net>
2021-11-01 18:45:21 +0100

    PNJ et dialogues

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+
+    public Animator animator;
+
+    private Queue<string> sentences;
+
+    public static DialogueManager instence;
+
+    // permet d'acceder a DialogueManager depuis n'importe qu'elle classe
+    private void Awake()
+    {
+        if (instence != null)
+        {
+            Debug.LogWarning("Il y a plus d'une instance de DialogueManager dans la scène");
+            return;
+        }
+
+        instence = this;
+
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        animator.SetBool("isOpen", true);
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    void EndDialogue()
+    {
+        animator.SetBool("isOpen", false);
+    }
+}
