dc55aecd3d5c1e607e4e56e1776fbf3d2cd263fc
Corentin TALOUR <corentin.talour@bts-malraux.net>
2021-11-02 02:32:19 +0100

    Ajout d'un inventaire

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,10 +1,17 @@
 using UnityEngine.UI;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
     public int coinCount;
     public Text coinsCountText;
+
+    public List<Item> content = new List<Item>();
+    public int contentCurrentIndex = 0;
+    public Image ItemImageUI;
+    public Text itemNameUI;
+    public Sprite emptyItemImage;
 
     public static Inventory instence;
 
@@ -18,6 +25,71 @@ public class Inventory : MonoBehaviour
         }
 
         instence = this;
+    }
+
+    private void Start()
+    {
+        UpdateInventoryUI();
+    }
+
+    public void ConsumeItem()
+    {
+        if(content.Count == 0)
+        {
+            return;
+        }
+
+        Item currentItem = content[contentCurrentIndex];
+        PlayerHealth.instence.HealPlayer(currentItem.hpGiven);
+        PlayerMovement.instence.moveSpeed += currentItem.speedGiven;
+        content.Remove(currentItem);
+        GetNextItem();
+        UpdateInventoryUI();
+    }
+
+    public void GetNextItem()
+    {
+        if (content.Count == 0)
+        {
+            return;
+        }
+
+        contentCurrentIndex++;
+        if (contentCurrentIndex > content.Count - 1)
+        {
+            contentCurrentIndex = 0;
+        }
+        UpdateInventoryUI();
+    }
+
+    public void GetPreviousItem()
+    {
+        if (content.Count == 0)
+        {
+            return;
+        }
+
+        contentCurrentIndex--;
+        if (contentCurrentIndex < 0)
+        {
+            contentCurrentIndex = content.Count - 1;
+        }
+        UpdateInventoryUI();
+    }
+
+    public void UpdateInventoryUI()
+    {
+        if(content.Count > 0)
+        {
+            ItemImageUI.sprite = content[contentCurrentIndex].image;
+            itemNameUI.text = content[contentCurrentIndex].name;
+        }
+        else
+        {
+            ItemImageUI.sprite = emptyItemImage;
+            itemNameUI.text = "";
+        }
+        
     }
 
     public void AddCoins(int count)
